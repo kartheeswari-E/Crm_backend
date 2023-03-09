11e2b71ab4739e23509b97d95f8a2c37c645dae9
kartheeswari <kartheesw2001@gmail.com>
2023-03-09 16:48:36 +0530

    last commit

--- index.js
+++ index.js
@@ -1,45 +1,94 @@
 const express = require("express");
 const db = require("./db/connect");
 const app = express();
+const mongoose = require('mongoose');
+const Razorpay= require("razorpay")
 const createuser = require("./route/createuser");
 const loginuser = require("./route/loginuser");
 const reset = require("./route/reset");
 const car = require("./route/carpost");
 const add = require("./route/add");
 const doubt = require("./route/createdoubt");
-const{v4:uuidv4}=require("uuid")
+
 const mentor = require("./route/creatementor");
-const stripe=require("stripe")(process.env.SECRET_KEY)
+
 const cors = require("cors");
 require("dotenv").config();
 db();
-app.use(express.json());
+app.use(express.json({ extended: false }));
 app.use(cors());
-const PORT = process.env.PORT;
+
+const OrderSchema = mongoose.Schema({
+  isPaid: Boolean,
+  amount: Number,
+  razorpay: {
+    orderId: String,
+    paymentId: String,
+    signature: String,
+  },
+});
+const Order = mongoose.model('Order', OrderSchema);
+
+app.get('api/get-razorpay-key', (req, res) => {
+  res.send({ key: process.env.RAZORPAY_KEY_ID });
+});
+
+app.post('api/create-order', async (req, res) => {
+  try {
+    const instance = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_SECRET,
+    });
+    const options = {
+      amount: req.body.amount,
+      currency: 'INR',
+    };
+    const order = await instance.orders.create(options);
+    if (!order) return res.status(500).send('Some error occured');
+    res.send(order);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+app.post('api/pay-order', async (req, res) => {
+  try {
+    const { amount, razorpayPaymentId, razorpayOrderId, razorpaySignature } =
+      req.body;
+    const newOrder = Order({
+      isPaid: true,
+      amount: amount,
+      razorpay: {
+        orderId: razorpayOrderId,
+        paymentId: razorpayPaymentId,
+        signature: razorpaySignature,
+      },
+    });
+    await newOrder.save();
+    res.send({
+      msg: 'Payment was successfull',
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+app.get('api/list-orders', async (req, res) => {
+  const orders = await Order.find();
+  res.send(orders);
+});
+
+
+
+
+const PORT = process.env.PORT || 6000;
 app.get("/", (request, response) => {
     response.send("Welcome to Crm Application");
   });
 
-  app.post('/api/payment',(req,res)=>{
-    const{data,token}=req.body;
-   const transkey=uuidv4();
-   return stripe.customers.create({
-    email:token.email,
-    source:token.id,
-   }).then((customer)=>{
-    stripe.charges.create({
-      amount:data.price,
-      currency:"inr",
-      customer:customer.id,
-      receipt_email:token.email,
-      description:data.name
-    }).then((result)=>{
-      res.json(result);
-    }).catch((err)=>{
-      console.log(err);
-    });
-   });
-  });
+
+
   app.use("/api/createuser", createuser);
   app.use("/api/reset", reset);
   app.use("/api/loginuser", loginuser);
